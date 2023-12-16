6bc3d8a003c2b44f426315e03b2cded195911254
hma-3 <maria.gula0307@gmail.com>
2023-12-17 01:33:43 +0200

    Initial commit

---
+++ Singleton.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Government {
+    Government() {}
+    Government(const Government&) = delete;
+    Government& operator=(const Government&) = delete;
+    string countryName;
+public:
+    static Government& getInstance() {
+        static Government instance;
+        return instance;
+    }
+
+    const string& getCountryName() const {
+        return countryName;
+    }
+
+    void setCountryName(const string& name) {
+        countryName = name;
+    } 
+};
+
+int main() {
+    Government& government = Government::getInstance();
+
+    government.setCountryName("Example Country");
+    cout << "Country Name: " << government.getCountryName() << endl;
+
+    return 0;
+}
